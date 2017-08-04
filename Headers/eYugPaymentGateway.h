3898760f6c236462529460cfee45445b703e5497
Hemendra Singh <hemendra.singh@e-yug.com>
2017-08-04 18:29:24 +0530

    Initial Commit

---
+++ Headers/eYugPaymentGateway.h
@@ -0,0 +1,19 @@
+//
+//  eYugPaymentGateway.h
+//  eYugPaymentGateway
+//
+//  Created by Eyugcom on 04/08/17.
+//  Copyright © 2017 Eyugcom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for eYugPaymentGateway.
+FOUNDATION_EXPORT double eYugPaymentGatewayVersionNumber;
+
+//! Project version string for eYugPaymentGateway.
+FOUNDATION_EXPORT const unsigned char eYugPaymentGatewayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <eYugPaymentGateway/PublicHeader.h>
+
+
